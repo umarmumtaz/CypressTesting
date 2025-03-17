1bc9f4bd43d85fadb3ccecf8b09bacd6fd9aa6eb
umarmumtaz <umarmumtaz.info@gmail.com>
2025-03-17 05:07:21 +0500

    Login Test Suite

--- cypress/e2e/Login.feature
+++ cypress/e2e/Login.feature
@@ -11,4 +11,12 @@ Scenario: Login with invalid login credentials
 Given I navigate to the login page 
 When I click on the login button
 When Allow Cookies
-Then User should be not be Login successfully
+Then User should be not be Login with empty credentials successfully
+
+Scenario: Login with invalid username and password credentials
+Given I navigate to the login page 
+When I click on the login button
+When Allow Cookies
+And I enter the invalid Email address
+And I enter the invalid Password
+Then User should be Login with invalid credentials successfully
--- cypress/support/step_definitions/Loginpage_Steps.js
+++ cypress/support/step_definitions/Loginpage_Steps.js
@@ -2,39 +2,56 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 const url = "https://beta.jobtrain.co.uk/Home/";
-// beforeEach(() => {
-//   cy.window().then((win) => {
-//     win.console.error = cy.stub(); // Suppress error logs
-//  
-  Given("I navigate to the login page", () => {
-    cy.visit(url);
-  });
-  When("I click on the login button", () => {
- cy.get('[data-test="a-sign-in"]').click();
-  });
-  When("Allow Cookies", () => {
-cy.get(".cc-allow").click();
-  });
-  When("I enter the Email address", () => {
-    cy.get('[data-testid="txt-email"]').type("nanncykevin+2024@gmail.com");
-  });
-  When("I enter the Password", () => {
-    cy.get('[data-testid="txt-password"]').type("Testing@123");
-  });
-  Then("User should be Login successfully", () => {
-    cy.get( '[data-testid="btn-Please enter your username and password to sign in"]').click();
-    cy.wait(500);
- //   cy.get('[data-testid="h1-jt-page-title"]').should('have.text','My Applieeecations');
- cy.get('[data-testid="h1-jt-page-title"]').should('have.text', 'My Applications');
-//cy.get('h1.h3.jt-page-title').invoke('text').then((text) => {
-//  console.log(text); // This will print the text to the console
-//});
+Given("I navigate to the login page", () => {
+  cy.visit(url);
+});
+When("I click on the login button", () => {
+  cy.get('[data-test="a-sign-in"]').click();
+});
+When("Allow Cookies", () => {
+  cy.get(".cc-allow").click();
+});
+When("I enter the Email address", () => {
+  cy.get('[data-testid="txt-email"]').type("nanncykevin+2024@gmail.com");
+});
+When("I enter the Password", () => {
+  cy.get('[data-testid="txt-password"]').type("Testing@123");
+});
+Then("User should be Login successfully", () => {
+  cy.get(
+    '[data-testid="btn-Please enter your username and password to sign in"]'
+  ).click();
+  cy.wait(500);
+  //   cy.get('[data-testid="h1-jt-page-title"]').should('have.text','My Applieeecations');
+  cy.get('[data-testid="h1-jt-page-title"]').should(
+    "have.text",
+    "My Applications"
+  );
+  //cy.get('h1.h3.jt-page-title').invoke('text').then((text) => {
+  //  console.log(text); // This will print the text to the console
+  //});
+});
 
+Then("User should be not be Login with empty credentials successfully", () => {
+  cy.get(
+    '[data-testid="btn-Please enter your username and password to sign in"]'
+  ).click();
+  cy.on("window:alert", (str) => {
+    expect(str).to.equal("Invalid UserName Or Password");
   });
+});
 
-  Then("User should be not be Login successfully", () => {
-    cy.on('window:alert', (str) => {
-        expect(str).to.equal('Invali');
-      });
-      
+When("I enter the invalid Email address", () => {
+  cy.get('[data-testid="txt-email"]').type("nanncykevin+invalid@gmail.com");
+});
+When("I enter the invalid Password", () => {
+  cy.get('[data-testid="txt-password"]').type("Testing22@123");
+});
+Then("User should be Login with invalid credentials successfully", () => {
+  cy.get(
+    '[data-testid="btn-Please enter your username and password to sign in"]'
+  ).click();
+  cy.on("window:alert", (str) => {
+    expect(str).to.equal("Invalid UserName Or Password");
   });
+});
